6d5605c89bb95b8108fbf315dc4bbc3940473fce
MrCoderMan134 <87577906+MrCoderMan134@users.noreply.github.com>
2021-11-19 19:54:14 -0500

    added .Java
    its a file, with java

---
+++ yeah bro i know Java now lesss goooo.java
@@ -0,0 +1,12 @@
+int x = 1;
+int y = 1;
+int yxTotal;
+
+
+yxTotal = y + x;
+
+
+System.out.PrintIn(yxTotal) // prints yxTotal (Which is intergers y + x!)
+
+
+// It doesn't show syntax errors for some reason lol i cant see if im doing something wrong, I bet i am lol
