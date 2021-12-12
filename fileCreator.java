35123585b8b00fa711e0d0551bce85e671cd720f
TrojanMan <87577906+PseudoTrojan@users.noreply.github.com>
2021-12-12 15:02:53 -0500

    Add files via upload
    Added another .Java file

---
+++ fileCreator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class Main {
+   public static void main(String[] args) {
+   printTwice("Hey!");
+    for (int i = 0; i<1; i++) {
+     File dataSaves = new File("saveData");
+     System.out.println("DataSave file created");
+     int 
+    }
+   
+  }
+ 
+ public static void printTwice(String fname) {
+    for (int i=0; i<2; i++) {
+     System.out.println(fname);
+    }
+   }
+  } 
