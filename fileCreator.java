36d9129aacedb58037263f92ff0b07917ac53ce4
TrojanMan <87577906+PseudoTrojan@users.noreply.github.com>
2021-12-16 16:48:08 -0500

    Update fileCreator.java
    Rewrote entire code, deleted stuff that I didn't need such as importing java.util.Random
    
    I used a real text editor this time woohoo the spacing should  be right!

--- fileCreator.java
+++ fileCreator.java
@@ -1,22 +1,18 @@
-import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
-import java.time.LocalDate;
 
 public class Main {
-   public static void main(String[] args) {
-   printTwice("Hey!");
-    for (int i = 0; i<1; i++) {
-     File dataSaves = new File("saveData");
-     System.out.println("DataSave file created");
-     int 
-    }
-   
-  }
- 
- public static void printTwice(String fname) {
-    for (int i=0; i<2; i++) {
-     System.out.println(fname);
-    }
+    public static void main(String[] args) {
+      for (int i=0; i<2; i++) {
+         File saveData = new File("saveData");
+         printTwice("Data saved!");
+      }
    }
-  } 
+ 
+ 
+   public static void printTwice(String fname) {
+      for (int i=0; i<2; i++) {
+         System.out.prinln(fname);
+      }
+   }
+}
